1f0158d253f3d3104799299d76121e0b8628fb3e
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-05-06 23:25:51 +1000

    Create components

--- VocaDbWeb/Scripts/App.tsx
+++ VocaDbWeb/Scripts/App.tsx
@@ -1,9 +1,43 @@
+import GlobalSearchBox from '@Components/Shared/GlobalSearchBox';
+import Footer from '@Components/Shared/Partials/Footer';
+import LeftMenu from '@Components/Shared/Partials/LeftMenu';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './i18n';
 
+const ErrorNotFound = React.lazy(
+	() => import('@Components/Error/ErrorNotFound'),
+);
+
 const App = (): React.ReactElement => {
-	return <></>;
+	return (
+		<BrowserRouter>
+			<div className="navbar navbar-inverse navbar-fixed-top">
+				<div className="navbar-inner">
+					<div id="topBar" className="container">
+						<GlobalSearchBox /* TODO */ />
+					</div>
+				</div>
+			</div>
+
+			<div className="container-fluid">
+				<div className="row-fluid">
+					<LeftMenu />
+
+					<div className="span10 rightFrame well">
+						<React.Suspense fallback={null /* TODO */}>
+							<Routes>
+								<Route path="/*" element={<ErrorNotFound />} />
+							</Routes>
+						</React.Suspense>
+					</div>
+				</div>
+			</div>
+
+			<Footer />
+		</BrowserRouter>
+	);
 };
 
 export default App;
--- VocaDbWeb/Scripts/Models/LoginManager.ts
+++ VocaDbWeb/Scripts/Models/LoginManager.ts
@@ -66,11 +66,19 @@ export default class LoginManager {
 		return this.loggedUser.effectivePermissions.includes(token);
 	};
 
+	public get canAccessManageMenu(): boolean {
+		return this.hasPermission(PermissionToken.AccessManageMenu);
+	}
+
 	public get canBulkDeletePVs(): boolean {
 		return this.hasPermission(PermissionToken.BulkDeletePVs);
 	}
 
 	public get canDeleteComments(): boolean {
 		return this.hasPermission(PermissionToken.DeleteComments);
+	}
+
+	public get canManageDatabase(): boolean {
+		return this.hasPermission(PermissionToken.ManageDatabase);
 	}
 }
